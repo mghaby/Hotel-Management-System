2af353036d65c371f92b18ae1f5af78eacefc636
mghaby <mark.ghaby71@hotmail.com>
2021-12-13 21:38:38 +1100

    frame impleneted and buttons added

--- App.java
+++ App.java
@@ -1,12 +1,98 @@
-
+import javax.swing.*;
+import java.awt.*;
 public class App {
+
+    private JFrame f;
+    private JPanel p;
+    private JButton plus;
+    private JButton minus;
+    private JButton times;
+    private JButton divide;
+    private JButton equals;
+    private JButton clear;
+    private JButton one;
+    private JButton two;
+    private JButton three;
+    private JButton four;
+    private JButton five;
+    private JButton six;
+    private JButton seven;
+    private JButton eight;
+    private JButton nine;
+    private JButton zero;
+    private JTextField t;
+    private JButton dot;
+    private JButton pcent;
+    private JButton sqrt;
+    private JButton sqr;
+    private JButton leftBracket;
+    private JButton rightBracket;
+
+    public App() {
+        f = new JFrame("Calculator");
+        p = new JPanel();
+        plus = new JButton("+");
+        minus = new JButton("-");
+        times = new JButton("*");
+        divide = new JButton("/");
+        equals = new JButton("=");
+        clear = new JButton("C");
+        one = new JButton("1");
+        two = new JButton("2");
+        three = new JButton("3");
+        four = new JButton("4");
+        five = new JButton("5");
+        six = new JButton("6");
+        seven = new JButton("7");
+        eight = new JButton("8");
+        nine = new JButton("9");
+        zero = new JButton("0");
+        t = new JTextField(20);
+        dot = new JButton(".");
+        pcent = new JButton("%");
+        sqrt = new JButton("sqrt");
+        sqr = new JButton("sqr");
+        leftBracket = new JButton("(");
+        rightBracket = new JButton(")");
+
+
+        p.add(plus);
+        p.add(minus);
+        p.add(times);
+        p.add(divide);
+        p.add(equals);
+        p.add(clear);
+        p.add(one);
+        p.add(two);
+        p.add(three);
+        p.add(four);
+        p.add(five);
+        p.add(six);
+        p.add(seven);
+        p.add(eight);
+        p.add(nine);
+        p.add(zero);
+        p.add(t);
+        p.add(dot);
+        p.add(pcent);
+        p.add(sqrt);
+        p.add(sqr);
+        p.add(leftBracket);
+        p.add(rightBracket);
+        
+        f.add(p);
+        f.setSize(700, 500);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) throws Exception {
 
         BasicCalculator basicCalculator = new BasicCalculator();
-        System.out.println(basicCalculator.add(1, 2, 3, 4, 5, 6));
-        //System.out.println("Hello World");
+       
+        App app = new App();
 
+        Thread.sleep(500);
 
 
     }
@@ -22,3 +108,6 @@ public class App {
 
 
 // figure out how to send stuff to the C file
+
+// make a method that takes a dynamic number of arguments and adds them all together
+// make a method that takes a dynamic number of arguments and subtracts them all together
--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,16 +1,13 @@
 import java.lang.Math;
 
 public class BasicCalculator implements Calculator {
-    int intValue;
-    double doubleValue;
 
     @Override
     public int add(int... numbers) {
         int sum = 0;
         for(int i = 0; i < numbers.length; i++) {
-           sum =+ numbers[i];
+           sum += numbers[i];
         }
-        intValue = sum;
         return sum;
     }
 
@@ -18,7 +15,7 @@ public class BasicCalculator implements Calculator {
     public double add(double... numbers) {
         double sum = 0.0;
         for(int i = 0; i < numbers.length; i++) {
-           sum =+ numbers[i];
+           sum += numbers[i];
         }
         return sum;
     }
@@ -27,7 +24,7 @@ public class BasicCalculator implements Calculator {
     public int sub(int... numbers) {
         int sum = 0;
         for(int i = 0; i < numbers.length; i++) {
-           sum =- numbers[i];
+           sum -= numbers[i];
         }
         return sum;
     }
@@ -36,7 +33,7 @@ public class BasicCalculator implements Calculator {
     public double sub(double... numbers) {
         double sum = 0.0;
         for(int i = 0; i < numbers.length; i++) {
-           sum =- numbers[i];
+           sum -= numbers[i];
         }
         return sum;
     }
@@ -45,7 +42,7 @@ public class BasicCalculator implements Calculator {
     public int mul(int... numbers) {
         int sum = 0;
         for(int i = 0; i < numbers.length; i++) {
-           sum = sum * numbers[i];
+           sum *= numbers[i];
         }
         return sum;
     }
@@ -54,7 +51,7 @@ public class BasicCalculator implements Calculator {
     public double mul(double... numbers) {
         double sum = 0.0;
         for(int i = 0; i < numbers.length; i++) {
-           sum = sum * numbers[i];
+           sum *= numbers[i];
         }
         return sum;
     }
@@ -63,7 +60,7 @@ public class BasicCalculator implements Calculator {
     public int div(int... numbers) {
         int sum = 0;
         for(int i = 0; i < numbers.length; i++) {
-           sum = sum / numbers[i];
+           sum /= numbers[i];
         }
         return sum;
     }
@@ -72,7 +69,7 @@ public class BasicCalculator implements Calculator {
     public double div(double... numbers) {
         double sum = 0.0;
         for(int i = 0; i < numbers.length; i++) {
-           sum = sum / numbers[i];
+           sum /= numbers[i];
         }
         return sum;
     }
@@ -90,13 +87,6 @@ public class BasicCalculator implements Calculator {
     @Override
     public double pow(double number, double power) {
         return Math.pow(number, power);
-    }
-
-    @Override
-    public String toString() { // ---------------------------------------------------------------- need to be fixed
-        // if loop to see if double = null then print int or if not just print double
-
-        return "" + intValue;
     }
 
 }
