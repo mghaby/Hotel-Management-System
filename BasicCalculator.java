98145bb7ba46ee8bc145da944ca358b90913a243
mghaby <mark.ghaby71@hotmail.com>
2021-12-13 15:00:12 +1100

    basic calculator math done

---
+++ BasicCalculator.java
@@ -0,0 +1,92 @@
+import java.lang.Math;
+
+public class BasicCalculator implements Calculator {
+
+    @Override
+    public int add(int... numbers) {
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum =+ numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public double add(double... numbers) {
+        double sum = 0.0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum =+ numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public int sub(int... numbers) {
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum =- numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public double sub(double... numbers) {
+        double sum = 0.0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum =- numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public int mul(int... numbers) {
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum = sum * numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public double mul(double... numbers) {
+        double sum = 0.0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum = sum * numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public int div(int... numbers) {
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum = sum / numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public double div(double... numbers) {
+        double sum = 0.0;
+        for(int i = 0; i < numbers.length; i++) {
+           sum = sum / numbers[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public double percent(double number) {
+        return number / 100;
+    }
+
+    @Override
+    public double sqrt(double number) {
+        return Math.sqrt(number);
+    }
+
+    @Override
+    public double pow(double number, double power) {
+        return Math.pow(number, power);
+    }
+
+}
--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,14 @@
 public interface Calculator{
-    
+    int add(int... numbers);
+    double add(double... numbers);
+    int sub(int... numbers);
+    double sub(double... numbers);
+    int mul(int... numbers);
+    double mul(double... numbers);
+    int div(int... numbers);
+    double div(double... numbers);
+    double percent(double number);
+    double sqrt(double number);
+    double pow(double number, double power);
+
 }
