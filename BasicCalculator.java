4bc99232dda819cee39574a493b2eb88b699950e
mghaby <mark.ghaby71@hotmail.com>
2021-12-13 21:56:16 +1100

    sorted jframe and jpannel into basic calcualtor

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,6 +1,93 @@
 import java.lang.Math;
+import javax.swing.*;
+import java.awt.*;
 
 public class BasicCalculator implements Calculator {
+
+    private JFrame f;
+    private JPanel p;
+    private JButton plus;
+    private JButton minus;
+    private JButton times;
+    private JButton divide;
+    private JButton equals;
+    private JButton clear;
+    private JButton one;
+    private JButton two;
+    private JButton three;
+    private JButton four;
+    private JButton five;
+    private JButton six;
+    private JButton seven;
+    private JButton eight;
+    private JButton nine;
+    private JButton zero;
+    private JButton dot;
+    private JButton pcent;
+    private JButton sqrt;
+    private JButton sqr;
+    private JButton leftBracket;
+    private JButton rightBracket;
+    private JTextField t;
+
+    String numberBuffer;
+
+    public BasicCalculator(){
+        f = new JFrame("Calculator");
+        p = new JPanel();
+        plus = new JButton("+");
+        minus = new JButton("-");
+        times = new JButton("*");
+        divide = new JButton("/");
+        equals = new JButton("=");
+        clear = new JButton("C");
+        one = new JButton("1");
+        two = new JButton("2");
+        three = new JButton("3");
+        four = new JButton("4");
+        five = new JButton("5");
+        six = new JButton("6");
+        seven = new JButton("7");
+        eight = new JButton("8");
+        nine = new JButton("9");
+        zero = new JButton("0");
+        t = new JTextField(20);
+        dot = new JButton(".");
+        pcent = new JButton("%");
+        sqrt = new JButton("sqrt");
+        sqr = new JButton("sqr");
+        leftBracket = new JButton("(");
+        rightBracket = new JButton(")");
+
+        p.add(plus);
+        p.add(minus);
+        p.add(times);
+        p.add(divide);
+        p.add(equals);
+        p.add(clear);
+        p.add(one);
+        p.add(two);
+        p.add(three);
+        p.add(four);
+        p.add(five);
+        p.add(six);
+        p.add(seven);
+        p.add(eight);
+        p.add(nine);
+        p.add(zero);
+        p.add(t);
+        p.add(dot);
+        p.add(pcent);
+        p.add(sqrt);
+        p.add(sqr);
+        p.add(leftBracket);
+        p.add(rightBracket);
+
+        f.add(p);
+        f.setSize(700, 500);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     @Override
     public int add(int... numbers) {
