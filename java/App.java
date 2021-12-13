4998416200ef5e29540b9b115a667655859dc8ff
mghaby <mark.ghaby71@hotmail.com>
2021-12-13 14:01:41 +1100

    pseudo code

---
+++ java/App.java
@@ -0,0 +1,18 @@
+package java;
+
+public class App {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+
+
+    }
+
+
+}
+
+// make a calculator interface that implements into basic calculator
+// make the calculator interface have all of the basic methods (add, subtract, multiply, divide, square root, square, brackets, clear and decimal point and percetage calcualtor)
+// make scientific calculator extend this class or others as best fit
+// make a programming mode as well
