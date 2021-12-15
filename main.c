5d4f0e1d0cfe9fd05ddc57ac7f36e4c02a5ec361
mghaby <mark.ghaby71@hotmail.com>
2021-12-15 15:45:10 +1100

    pseudo coding

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+
+    return 0;
+}
+
+// Have an ASCII UI that asks if you want to book a room, check out of a room or go in a room.
+// If you want to book a room it will return what rooms are available and you can select a room. 
+    // After you select a room it will give you a randomly generated password
+
+// If you want to go inside a room you have to enter what room and the password.
+    // If you enter the wrong password you will be told that you have entered the wrong password.
+    // If you enter the correct password you will be able to enter the room.
+
+// If you want to check out a room you must enter the room number and the password and the room will be checked out.
+    // If you enter the wrong password you will be told that you have entered the wrong password.
+    // If you enter the correct password you will be told that you have checked out of the room.
+
+// The "Database" will be a text file or JSON file that will hold the room numbers and the status of the rooms.
